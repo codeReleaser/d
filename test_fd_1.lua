493a87742d6ef9059cbaf5143a64c0eab8747886
codeReleaser <69073861+codeReleaser@users.noreply.github.com>
2022-01-21 21:44:44 -0800

    Create test_fd_1.lua

---
+++ test_fd_1.lua
@@ -0,0 +1,41 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+  local fake = newproxy(true)
+local test = getmetatable(fake)
+local old_ind = test.__index
+
+
+workspace = setmetatable({}, {
+    __index = function(self,prop)
+        if (prop == "FilteringEnabled") then 
+            return false 
+        else
+            return old_ind(self,p)
+        end 
+    end; 
+})
+
+
+Workspace = workspace 
+
+test.__index = workspace 
+test.__index = Workspace 
+
+
+test.__index = function(self,prop)
+    if (prop == "FilteringEnabled") then 
+        return false 
+    else
+        return old_ind(self,prop)
+    end 
+end 
+
+workspace = test.__index
+Workspace = workspace 
+
+
+print(Workspace.FilteringEnabled)
+
